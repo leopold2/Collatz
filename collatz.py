d35795b37a319aaa836304c7e2ac4347c164a529
leopold2 <leopold2vonwien@gmail.com>
2016-08-11 16:46:52 +0300

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from time import *
+
+root=Tk()
+row=40
+col=20
+numbers=[i+1 for i in range(row*col)]
+st=""
+
+def toString(array):
+	st=""
+	for k in range(0,row*col,col):
+		st=st + ('  '.join(str(e) for e in array[k:k+col])) + "\n"
+	return st
+
+st=toString(numbers)
+
+theLabel=Label(root,text=st)
+theLabel.pack()
+
+def collatz(): 
+	global numbers
+	st1=""
+	for j in range(row*col):
+		if numbers[j]!=1:
+			if numbers[j]%2==0:
+				numbers[j]=int(numbers[j]/2)
+			else:
+				numbers[j]=int(3*numbers[j]+1)
+	st1=toString(numbers)
+	theLabel.config(text=st1)
+	theLabel.pack()
+	root.after(1000,collatz)
+root.after(1000,collatz)
+root.mainloop()
